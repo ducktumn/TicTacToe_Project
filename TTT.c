3bb386d7bf1a6611c4ef3aa34ee47aa4f4fd2a5f
Ali Egemen Bilak <aliegemenbilakprogrammer@gmail.com>
2024-12-27 00:12:00 +0300

    Last working version. Added bunch of stuff.

--- TTT.c
+++ TTT.c
@@ -1,17 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <math.h>
 
-// Hej hej hej sokoły :D
+/*  Written by Ali Egemen Bilak using VSCode and JDK 21.0.5 on Ubuntu 24.04.1 LTS
+    https://github.com/ducktumn
+    Last Edit: 26.12.2024
+    Made for MEF University COMP205 final project.
+*/
+
+#define RESET "\033[0m"
+#define ORANGE_BG "\033[48;5;202m"
+#define WHITE_TEXT "\033[97m"
+#define CYAN_BG "\033[46m"
+#define WHITE_BG "\033[47m"
+#define BOLD "\033[1m"
+#define RED_TEXT "\033[31m"
+#define YELLOW "\033[1;33m"
+
+// Hej hej hej sokoły
 // https://open.spotify.com/track/16okyAWAlUFsNacLxukOsp?si=5762286771f948a4
 
 char *getBinary(unsigned long int number, unsigned short int bitCount);
 void printGameTable(unsigned short int size, unsigned long int xStatus, unsigned long int oStatus);
-int checkWin(unsigned long int status, unsigned short int size);
+int checkWin(unsigned long int status, unsigned long int *possibilities, unsigned short int countOfPossibilities);
 void clearBuffer();
 int isMoveValid(unsigned short int size, unsigned short int x, unsigned short int y, unsigned long int xState, unsigned long int oState, unsigned short int current);
 void printResult(unsigned short int result);
+void makeMove(unsigned long int *status, unsigned short int row, unsigned short int col, unsigned short int size);
+void preCalculateMoves(unsigned long int **possibilities, unsigned short int size, unsigned short int *countOfPossibilities);
 
 int main()
 {
@@ -19,47 +35,91 @@ int main()
     unsigned long int stateOfO = 0;
 
     unsigned short int size = 0;
-    printf("Enter the size of the game (Maximum is 8, Minimum is 3) : ");
+    printf("%sEnter the size of the game (Maximum is 8, Minimum is 3) : %s", WHITE_TEXT, RESET);
     scanf("%hu", &size);
     clearBuffer();
     if (size < 3)
     {
-        printf("\nInvalid board size!\n");
+        printf("\n%sInvalid board size!%s\n", RED_TEXT, RESET);
         return 1;
     }
 
-    unsigned short int current = 0;
-    printf("Enter the first player (0 for X, 1 for O) : ");
+    unsigned long int *winConditions;
+    unsigned short int winConditionCount;
+    preCalculateMoves(&winConditions, size, &winConditionCount);
+    unsigned long int *preMoves = (unsigned long int *)malloc(sizeof(unsigned long int) * size * size);
+    if (preMoves == NULL)
+    {
+        printf("%sMemory Allocation Fail!%s\n", RED_TEXT, RESET);
+        return 1;
+    }
+
+        unsigned short int current = 0;
+    printf("%sEnter the first player (0 for X, 1 for O) : %s", WHITE_TEXT, RESET);
     scanf("%hu", &current);
     if (current != 1 && current != 0)
         current = 0;
     clearBuffer();
+    if (current == 0)
+        preMoves[0] = 0UL;
+    else
+        preMoves[0] = 0UL;
 
     unsigned short int moveCount = 0;
     unsigned short int gameOver = 0;
     while ((moveCount < size * size) && !gameOver)
     {
-        moveCount++;
+
         char player = 'O';
         if (current == 0)
             player = 'X';
 
+        unsigned short int decision;
+        printf("\n%sWhat would you like to do?\nMove - 0, Undo - 1: %s", WHITE_TEXT, RESET);
+        scanf("%hu", &decision);
+        clearBuffer();
+        if (decision && (moveCount > 0))
+        {
+            if (current == 0)
+            {
+                current = 1;
+                stateOfO = preMoves[moveCount - 2];
+                moveCount--;
+                printGameTable(size, stateOfX, stateOfO);
+            }
+            else
+            {
+                current = 0;
+                stateOfX = preMoves[moveCount - 1];
+                moveCount--;
+                printGameTable(size, stateOfX, stateOfO);
+            }
+            continue;
+        }
+        else if (decision && (moveCount == 0))
+        {
+            printf("%sCan't undo further!%s\n", RED_TEXT, RESET);
+            continue;
+        }
+
         unsigned short int row;
         unsigned short int col;
-        printf("\nEnter your move for \"%c\" by typing row and column with a space in the middle: ", player);
+        printf("\n%sEnter your move for \"%c\" by typing row and column with a space in the middle: %s", WHITE_TEXT, player, RESET);
         scanf("%hu%hu", &row, &col);
         clearBuffer();
         while (!isMoveValid(size, row, col, stateOfX, stateOfO, current))
         {
-            printf("Invalid input!\nEnter your move for \"%c\" by typing row and column with a space in the middle: ", player);
+            printf("\n%sInvalid input!\n%sEnter your move for \"%c\" by typing row and column with a space in the middle: %s", RED_TEXT, WHITE_TEXT, player, RESET);
             scanf("%hu%hu", &row, &col);
             clearBuffer();
         }
 
         if (current == 0)
         {
-            stateOfX += (unsigned long int)pow(2, (row - 1) * size + col - 1);
-            if (checkWin(stateOfX, size))
+            makeMove(&stateOfX, row, col, size);
+            preMoves[moveCount + 1] = stateOfX;
+            moveCount++;
+            if (checkWin(stateOfX, winConditions, winConditionCount))
                 gameOver = 1;
             else
                 current = 1;
@@ -67,8 +127,10 @@ int main()
 
         else
         {
-            stateOfO += (unsigned long int)pow(2, (row - 1) * size + col - 1);
-            if (checkWin(stateOfO, size))
+            makeMove(&stateOfO, row, col, size);
+            preMoves[moveCount + 1] = stateOfO;
+            moveCount++;
+            if (checkWin(stateOfO, winConditions, winConditionCount))
                 gameOver = 1;
             else
                 current = 0;
@@ -91,98 +153,46 @@ int main()
 void printGameTable(unsigned short int size, unsigned long int xStatus, unsigned long int oStatus)
 {
     printf("\n");
+    printf("   ");
     for (int i = 0; i < size; i++)
     {
-        char *tempString = (char *)malloc(size + 1);
-        tempString[size] = '\0';
+        printf("%s%s %d %s", BOLD, WHITE_TEXT, i + 1, RESET);
+    }
+    printf("\n");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%s%s %d %s", BOLD, WHITE_TEXT, i + 1, RESET);
         for (int j = 0; j < size; j++)
         {
             if ((xStatus & 1) == 1)
-                tempString[j] = 'X';
+            {
+                printf("%s%s X %s", ORANGE_BG, WHITE_TEXT, RESET);
+            }
             else if ((oStatus & 1) == 1)
-                tempString[j] = 'O';
+            {
+                printf("%s%s O %s", CYAN_BG, WHITE_TEXT, RESET);
+            }
             else
-                tempString[j] = '-';
+            {
+                printf("%s   %s", WHITE_BG, RESET);
+            }
+
             xStatus = xStatus >> 1;
             oStatus = oStatus >> 1;
         }
-        printf("%s\n", tempString);
-        free(tempString);
+
+        printf("\n");
     }
     printf("\n");
 }
 
-int checkWin(unsigned long int status, unsigned short int size)
+int checkWin(unsigned long int status, unsigned long int *possibilities, unsigned short int countOfPossibilities)
 {
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < countOfPossibilities; i++)
     {
-        unsigned long int horizontalCheck = (unsigned long int)(pow(2, i * size) * 7);
-        unsigned long int tempVerticalR = (unsigned long int)pow(2, size);
-        unsigned long int verticalCheck = (unsigned long int)(pow(2, i) * (pow(tempVerticalR, 3) - 1) / (tempVerticalR - 1));
-        for (int j = 0; j < size - 2; j++)
-        {
-            if ((horizontalCheck & status) == horizontalCheck)
-                return 1;
-            horizontalCheck << 1;
-        }
-        for (int j = 0; j < size - 2; j++)
-        {
-            if ((verticalCheck & status) == verticalCheck)
-                return 1;
-            verticalCheck << size;
-        }
+        if ((possibilities[i] & status) == possibilities[i])
+            return 1;
     }
-
-    {
-        unsigned long int tempDiagonalR = (unsigned long int)pow(2, size + 1);
-        unsigned long int rightDiagonalCheck = (unsigned long int)((pow(tempDiagonalR, 3) - 1) / tempDiagonalR - 1);
-        for (int i = 0; i < (size - 2); i++)
-        {
-            if ((rightDiagonalCheck & status) == rightDiagonalCheck)
-                return 1;
-            rightDiagonalCheck << (size + 1);
-        }
-        for (int i = 0; i < (size - 3); i++)
-        {
-            unsigned long int subRightDiagonalCheck = (unsigned long int)(pow(2, i + 1) * (pow(tempDiagonalR, 3) - 1) / (tempDiagonalR - 1));
-            unsigned long int secondSubRightDiagonalCheck = (unsigned long int)(pow(2, (i + 1) * size) * (pow(tempDiagonalR, 3) - 1) / (tempDiagonalR - 1));
-            for (int j = 0; j < size - 3 - i; j++)
-            {
-                if ((subRightDiagonalCheck & status) == subRightDiagonalCheck)
-                    return 1;
-                if ((secondSubRightDiagonalCheck & status) == secondSubRightDiagonalCheck)
-                    return 1;
-                subRightDiagonalCheck << (size + 1);
-                secondSubRightDiagonalCheck << (size + 1);
-            }
-        }
-    }
-
-    {
-        unsigned long int tempDiagonalR = (unsigned long int)pow(2, size - 1);
-        unsigned long int leftDiagonalCheck = (unsigned long int)(pow(2, size - 1) * ((pow(tempDiagonalR, 3) - 1) / tempDiagonalR - 1));
-        for (int i = 0; i < (size - 2); i++)
-        {
-            if ((leftDiagonalCheck & status) == leftDiagonalCheck)
-                return 1;
-            leftDiagonalCheck << (size - 1);
-        }
-        for (int i = 0; i < (size - 3); i++)
-        {
-            unsigned long int subLeftDiagonalCheck = (unsigned long int)(pow(2, (size - 2 - i)) * (pow(tempDiagonalR, 3) - 1) / (tempDiagonalR - 1));
-            unsigned long int secondSubLeftDiagonalCheck = (unsigned long int)(pow(2, (i + 2) * size - 1) * (pow(tempDiagonalR, 3) - 1) / (tempDiagonalR - 1));
-            for (int j = 0; j < size - 3 - i; j++)
-            {
-                if ((subLeftDiagonalCheck & status) == subLeftDiagonalCheck)
-                    return 1;
-                if ((secondSubLeftDiagonalCheck & status) == secondSubLeftDiagonalCheck)
-                    return 1;
-                subLeftDiagonalCheck << (size - 1);
-                secondSubLeftDiagonalCheck << (size - 1);
-            }
-        }
-    }
-
     return 0;
 }
 
@@ -191,7 +201,8 @@ char *getBinary(unsigned long int number, unsigned short int bitCount)
     char *returnPointer = (char *)malloc(bitCount + 1);
     if (returnPointer == NULL)
     {
-        return NULL;
+        printf("%sMemory Allocation Fail!%s\n", RED_TEXT, RESET);
+        exit(1);
     }
 
     for (int i = bitCount - 1; i >= 0; i--)
@@ -218,7 +229,9 @@ int isMoveValid(unsigned short int size, unsigned short int x, unsigned short in
 {
     if ((x <= size) && (y <= size))
     {
-        unsigned long int move = (unsigned long int)pow(2, (x - 1) * size + y - 1);
+        unsigned short int tempShiftCount = (x - 1) * size + y - 1;
+        unsigned long int move = 1UL << tempShiftCount;
+
         if (((xState & move) == 0) && ((oState & move) == 0))
             return 1;
     }
@@ -230,15 +243,131 @@ void printResult(unsigned short int result)
     switch (result)
     {
     case 0:
-        printf("\n\"X\" won!!!\n");
+        printf("\n\"%sX\" won!!!%s\n", YELLOW, RESET);
         break;
     case 1:
-        printf("\n\"O\" won!!!\n");
+        printf("\n\"%sO\" won!!!%s\n", YELLOW, RESET);
         break;
     case 2:
-        printf("It's a draw!!!\n");
+        printf("\n%sIt's a draw!!!%s\n", YELLOW, RESET);
         break;
     default:
         break;
     }
+}
+
+void makeMove(unsigned long int *status, unsigned short int row, unsigned short int col, unsigned short int size)
+{
+    int index_a = ((row - 1) * size + col);
+    unsigned long int k = 1;
+    *status |= (k << (index_a - 1));
+}
+
+void preCalculateMoves(unsigned long int **possibilities, unsigned short int size, unsigned short int *countOfPossibilities)
+{
+    unsigned short int moveCount = size * (134 + size * (-38 + 4 * size)) - 160;
+    *countOfPossibilities = moveCount;
+    unsigned short int movesLeft = moveCount;
+
+    unsigned long int *winConditions = (unsigned long int *)malloc(sizeof(unsigned long int) * moveCount);
+
+    if (winConditions == NULL)
+    {
+        printf("%sMemory Allocation Fail!%s\n", RED_TEXT, RESET);
+        exit(1);
+    }
+    unsigned long int bit = 1;
+    {
+        unsigned long int tempHorizontal = (unsigned long int)7;
+        unsigned long int tempVertical = bit + (bit << size) + (bit << (2 * size));
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size - 2; j++)
+            {
+                winConditions[moveCount - movesLeft] = tempHorizontal;
+                movesLeft--;
+                winConditions[moveCount - movesLeft] = tempVertical;
+                movesLeft--;
+                if (j != (size - 3))
+                {
+                    tempHorizontal <<= 1;
+                    tempVertical <<= size;
+                }
+            }
+            tempHorizontal <<= 3;
+            if (size == 3)
+                tempVertical <<= 1;
+            else
+                tempVertical >>= ((size - 3) * size - 1);
+        }
+    }
+    {
+        unsigned long int firstMainDiagonal = 1 + (bit << (size + 1)) + (bit << (size + 1) << (size + 1));
+        unsigned long int secondMainDiagonal = (bit << (size - 1)) + (bit << (2 * size - 2)) + (bit << (3 * size - 3));
+        for (int i = 0; i < size - 2; i++)
+        {
+            winConditions[moveCount - movesLeft] = firstMainDiagonal;
+            movesLeft--;
+            winConditions[moveCount - movesLeft] = secondMainDiagonal;
+            movesLeft--;
+            firstMainDiagonal <<= (size + 1);
+            secondMainDiagonal <<= (size - 1);
+        }
+    }
+    {
+        unsigned long int tempUpSubDiagonal = (bit << (2 * size)) + (bit << (size + 1)) + (bit << 2);
+        unsigned long int tempDownSubDiagonal = (bit << (size - 3)) + (bit << (2 * size - 2)) + (bit << (3 * size - 1));
+
+        unsigned short int ascending = 1;
+        for (int i = 0; i < 2 * size - 5; i++)
+        {
+            if (i == (size - 3))
+            {
+                ascending = 0;
+                unsigned long int firstDescendingBit = bit << (size * (size - 1) + 1);
+                tempUpSubDiagonal = firstDescendingBit + (firstDescendingBit >> (size - 1)) + (firstDescendingBit >> (2 * size - 2));
+                firstDescendingBit <<= (size - 3);
+                tempDownSubDiagonal = firstDescendingBit + (firstDescendingBit >> (size + 1)) + (firstDescendingBit >> (2 * size + 2));
+            }
+            else if (ascending)
+            {
+                for (int j = 0; j < i + 1; j++)
+                {
+                    winConditions[moveCount - movesLeft] = tempUpSubDiagonal;
+                    movesLeft--;
+                    winConditions[moveCount - movesLeft] = tempDownSubDiagonal;
+                    movesLeft--;
+                    if (j != i)
+                    {
+                        tempUpSubDiagonal >>= (size - 1);
+                        tempDownSubDiagonal >>= (size + 1);
+                    }
+                }
+                unsigned long int ascendingBit = bit << ((3 + i) * size);
+                tempUpSubDiagonal = ascendingBit + (ascendingBit >> (size - 1)) + (ascendingBit >> (2 * size - 2));
+                ascendingBit = bit << ((4 + i) * size - 1);
+                tempDownSubDiagonal = ascendingBit + (ascendingBit >> (size + 1)) + (ascendingBit >> (2 * size + 2));
+            }
+            else
+            {
+                for (int j = 0; j < ((2 * size - 5) - i); j++)
+                {
+                    winConditions[moveCount - movesLeft] = tempUpSubDiagonal;
+                    movesLeft--;
+                    winConditions[moveCount - movesLeft] = tempDownSubDiagonal;
+                    movesLeft--;
+                    if (j != ((2 * size - 6) - i))
+                    {
+                        tempUpSubDiagonal >>= (size - 1);
+                        tempDownSubDiagonal >>= (size + 1);
+                    }
+                }
+                unsigned long int ascendingBit = bit << ((size - 1) * size + i - (size - 4));
+                tempUpSubDiagonal = ascendingBit + (ascendingBit >> (size - 1)) + (ascendingBit >> (2 * size - 2));
+                ascendingBit = bit << ((size - 1) * size + (2 * size - 5 - i));
+                tempDownSubDiagonal = ascendingBit + (ascendingBit >> (size + 1)) + (ascendingBit >> (2 * size + 2));
+            }
+        }
+    }
+    *possibilities = winConditions;
 }
